d7aa9f37f3813e594cc82534b2a31a0a7afcbb48
Þorvarður Örn Einarsson <thorrik58@gmail.com>
2020-05-19 11:43:15 +0000

    boilerplate set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import styles from "./App.module.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className={styles.app_header}>
+        <img src={logo} className={styles.app_logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles.app_link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
